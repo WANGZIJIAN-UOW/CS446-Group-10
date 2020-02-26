c53a48da52df6bee9729d19b77ae667de8912ecd
huangshuangyou <s257huan@edu.uwaterloo.ca>
2020-02-25 21:30:50 -0500

    fixed a ConfirmLoan bug

--- app/src/main/java/com/example/myapplication/AddLoan.java
+++ app/src/main/java/com/example/myapplication/AddLoan.java
@@ -217,7 +217,9 @@ public class AddLoan extends AppCompatActivity {
             public void onClick(View v) {
                 loanName = (EditText) findViewById(R.id.loanName);
                 loanMoney = (EditText) findViewById(R.id.loanMoney);
-                addNewLoan2db(username,loanName.getText().toString(), loanMoney.getText().toString());
+                if(!loanName.getText().toString().isEmpty() && !loanMoney.getText().toString().isEmpty()) {
+                    addNewLoan2db(username, loanName.getText().toString(), loanMoney.getText().toString());
+                }
             }
         });
 
